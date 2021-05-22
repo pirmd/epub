e0a8b539f56a48dc7d0246c732351dd972512c63
Πrmd <pirmd@users.noreply.github.com>
2021-05-22 11:21:57 +0200

    Refactor zip reader size calculation

--- ziputil.go
+++ ziputil.go
@@ -26,11 +26,23 @@ func openFromZip(r readatSeeker, path string) (io.ReadCloser, error) {
 }
 
 func newZipReader(r readatSeeker) (*zip.Reader, error) {
-	size, err := r.Seek(0, io.SeekEnd)
+	size, err := getSize(r)
 	if err != nil {
 		return nil, err
 	}
-	r.Seek(0, io.SeekStart)
 
 	return zip.NewReader(r, size)
+}
+
+func getSize(f io.Seeker) (int64, error) {
+	sz, err := f.Seek(0, io.SeekEnd)
+	if err != nil {
+		return 0, err
+	}
+
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
+		return 0, err
+	}
+
+	return sz, nil
 }

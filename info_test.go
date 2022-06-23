2af2445ceb7cb14e16bab11ec5c5e7cebff709cd
Πrmd <pirmd@users.noreply.github.com>
2022-06-23 07:34:48 +0200

    Refactor package structure for better readability

--- info_test.go
+++ info_test.go
@@ -8,23 +8,19 @@ import (
 	"github.com/pirmd/verify"
 )
 
-const (
-	testdataPath = "./testdata"
-)
-
-func TestGetPackageFromFile(t *testing.T) {
+func TestGetMetadataFromFile(t *testing.T) {
 	testCases, err := filepath.Glob(filepath.Join(testdataPath, "*.epub"))
 	if err != nil {
 		t.Fatalf("cannot read test data in %s:%v", testdataPath, err)
 	}
 
-	out := []*PackageDocument{}
+	out := []*Information{}
 	for _, tc := range testCases {
-		opf, err := GetPackageFromFile(tc)
+		m, err := GetMetadataFromFile(tc)
 		if err != nil {
-			t.Errorf("Fail to get package for %s: %v", tc, err)
+			t.Errorf("Fail to get metadata for %s: %v", tc, err)
 		}
-		out = append(out, opf)
+		out = append(out, m)
 	}
 
 	got, err := json.MarshalIndent(out, "", "  ")
@@ -33,6 +29,6 @@ func TestGetPackageFromFile(t *testing.T) {
 	}
 
 	if failure := verify.MatchGolden(t.Name(), string(got)); failure != nil {
-		t.Fatalf("Package Document is not as expected:\n%v", failure)
+		t.Fatalf("Metadata is not as expected:\n%v", failure)
 	}
 }
